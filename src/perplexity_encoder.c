328ff6bdfe0e4ef7dd25cf98273d6342b7a1fbcc
blackle <isabelle@blackle-mori.com>
2020-03-23 04:18:06 -0400

    Use faster log2 implementation thanks to stackoverflow answer

--- src/perplexity_encoder.c
+++ src/perplexity_encoder.c
@@ -1,10 +1,21 @@
 #include "perplexity_encoder.h"
 #include <math.h>
 
+//https://stackoverflow.com/a/28730362
+static float mFast_Log2(float val) {
+	union { float val; int32_t x; } u = { val };
+	register float log_2 = (float)(((u.x >> 23) & 255) - 128);
+	u.x   &= ~(255 << 23);
+	u.x   += 127 << 23;
+	log_2 += ((-0.34484843f) * u.val + 2.02466578f) * u.val - 0.67487759f;
+	return (log_2);
+}
+
 static void perplexity_encoder_encode_bit(EncoderInterface* enc, bool bit, Prob prob)
 {
+	//todo: use fixed point instead of float for speed reasons
 	float* perplexity = (float*)enc->private_data;
-	*perplexity += -log2(bit ? (1.f - prob/2048.f) : (prob/2048.f));
+	*perplexity += -mFast_Log2(bit ? (1.f - prob/2048.f) : (prob/2048.f));
 }
 
 static void perplexity_encoder_encode_direct_bits(EncoderInterface* enc, unsigned bits, unsigned num_bits)
--- tests/main.c
+++ tests/main.c
@@ -4,6 +4,7 @@ int main(int argc, char** argv)
 {
 	(void) argc; (void) argv;
 
+	//todo: substring_enumerator_test();
 	max_heap_test();
 
 	return 0;
